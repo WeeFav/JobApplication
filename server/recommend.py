7c2c6bca28d03da9167c1d38d3de77eb84508c40
Marvin <lim343@purdue.edu>
2025-02-06 20:36:56 -0500

    have a working recommedation system!

--- server/recommend.py
+++ server/recommend.py
@@ -11,6 +11,7 @@ import mysql.connector
 from langchain_chroma import Chroma
 import chromadb
 from langchain_ollama import OllamaEmbeddings
+import argparse
 load_dotenv()
 
 def extract_skill(jd):
@@ -23,10 +24,11 @@ def extract_skill(jd):
   return res
 
 def dump_vector_store(vector_store):
+  print("dumping vector store...")
   documents = vector_store._collection.get()
   ids = documents["ids"]  # List of IDs
   texts = documents["documents"]  # List of texts
-  print(vector_store._collection.count())
+  print(f"vector store currently have {vector_store._collection.count()} documents")
   df = pd.DataFrame({'job_id': ids, 'job_description': texts})
   df.to_csv(f"./chroma_db/{vector_store._collection_name}.csv", index=False)
 
@@ -51,6 +53,7 @@ cursor = db.cursor(dictionary=True)
 # setup LLM
 model_LLM = OllamaLLM(model="llama3.2")
 model_emb = OllamaEmbeddings(model="nomic-embed-text")
+
 # setup vector store
 chroma_client = chromadb.PersistentClient("./chroma_db")
 vector_store = Chroma(
@@ -58,63 +61,75 @@ vector_store = Chroma(
     collection_name="jd_collection",
     embedding_function=model_emb
 )
-vector_store.reset_collection()
 
-print("Python Server Runinng...")
-
-wait_next_data = True
-
-while True:
-  if wait_next_data:
-    print("waiting on data...")
-  result = r.brpop('queue', timeout=5)
-  if result:
-    print("recieved data")
-    _, data = result
-    jd_list = json.loads(data)
-    extracted_skill_list = []
-    id_list = []
-    
-    # extract all job description
-    for idx, job in enumerate(jd_list):
-      print(f"extracting job {job['job_id']} | {idx+1}/{len(jd_list)}")
-      skill = extract_skill(job['job_description'])
-      skill = "\n".join([x[2:] for x in skill.splitlines()])
-      extracted_skill_list.append(skill)
-      id_list.append(str(job['job_id']))
+def main():
+  wait_next_data = True
+  print("Python Server Runinng...")
+  
+  while True:
+    if wait_next_data:
+      print("waiting on data...")
+    result = r.brpop('queue', timeout=5)
+    if result:
+      print("recieved data")
+      _, data = result
+      jd_list = json.loads(data)
+      extracted_skill_list = []
+      id_list = []
       
-      # let machine cool down
-      for i in range(4, -1, -1):
-        print(f"cooling down... {i}", end=' \r')
-        time.sleep(1)
-      print('')
+      # extract all job description
+      for idx, job in enumerate(jd_list):
+        print(f"extracting job {job['job_id']} | {idx+1}/{len(jd_list)}")
+        skill = extract_skill(job['job_description'])
+        skill = "\n".join([x[2:] for x in skill.splitlines()])
+        extracted_skill_list.append(skill)
+        id_list.append(str(job['job_id']))
+        
+        # let machine cool down
+        for i in range(4, -1, -1):
+          print(f"cooling down... {i}", end=' \r')
+          time.sleep(1)
+        print('')
+      
+      updates = []
+      for i in range(len(extracted_skill_list)):
+        updates.append(f"""WHEN job_id = {id_list[i]} THEN "{extracted_skill_list[i]}" """)  
+      updates = "\n".join(updates)
+      job_id_list = "(" + ", ".join(id_list) + ")"
+      
+      print("updating database...")
+      cursor.execute(f"""
+                    UPDATE jobs
+                    SET job_extracted_skill = CASE 
+                    {updates}
+                    END
+                    WHERE job_id IN {job_id_list};
+                    """)
+      db.commit()
+      
+      # vectorize
+      print("vectorizing...")
+      vector_store.add_texts(texts=extracted_skill_list, ids=id_list)
+      
+      dump_vector_store(vector_store)
+      
+      wait_next_data = True
+    else:
+      wait_next_data = False
     
-    updates = []
-    for i in range(len(extracted_skill_list)):
-      updates.append(f"""WHEN job_id = {id_list[i]} THEN "{extracted_skill_list[i]}" """)  
-    updates = "\n".join(updates)
-    job_id_list = "(" + ", ".join(id_list) + ")"
-    
-    print("updating database...")
-    cursor.execute(f"""
-                   UPDATE jobs
-                   SET job_extracted_skill = CASE 
-                   {updates}
-                   END
-                   WHERE job_id IN {job_id_list};
-                   """)
-    db.commit()
-    
-    # vectorize
-    print("vectorizing...")
-    vector_store.add_texts(texts=extracted_skill_list, ids=id_list)
-    
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--reset", action="store_true", help="Reset vector store")
+  parser.add_argument("--recommend", action="store_true", help="Recommend only")
+  args = parser.parse_args()
+  
+  if args.reset:
+    vector_store.reset_collection()
+    print("Reset vector store")
+  elif args.recommend:
     # compute similarity
     print("recommending...")
-    results = vector_store.similarity_search("risk mangement", k=3)
+    results = vector_store.similarity_search("artificial intelligence, software engineer", k=10)
     print([x.id for x in results])
-    dump_vector_store(vector_store)
-    
-    wait_next_data = True
   else:
-    wait_next_data = False
+    main()
